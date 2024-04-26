47247b69b967ee06876e4a030e6280468320c378
HasnatShahriar <shahriarhriday94@gmail.com>
2024-04-27 02:12:09 +0600

    added not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>not found page</h2>
+    <div className="text-center">
+      <h1 className="text-[200px] font-bold">404</h1>
+      <h2 className="text-4xl font-bold">Not Found</h2>
+      <Link to='/' className=" text-2xl"><button className="btn btn-outline mt-16">Go Back</button></Link>
     </div>
   );
 };
