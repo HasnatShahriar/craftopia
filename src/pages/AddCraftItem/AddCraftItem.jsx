d0b1e8240be50c0fd821324be28ea4592e40ee64
HasnatShahriar <shahriarhriday94@gmail.com>
2024-04-27 15:39:22 +0600

    send data to the server

--- src/pages/AddCraftItem/AddCraftItem.jsx
+++ src/pages/AddCraftItem/AddCraftItem.jsx
@@ -1,9 +1,45 @@
 
 const AddCraftItem = () => {
+
+  const handleAddCraft = e =>{
+    e.preventDefault();
+    const form = e.target;
+    const photo = form.photo.value;
+    const item = form.item.value;
+    const subCategory = form.subCategory.value;
+    const description = form.description.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const customization = form.customization.value;
+    const processing = form.processing.value;
+    const stock = form.stock.value;
+    const email = form.email.value;
+    const name = form.name.value;
+
+    const newCraft = {photo,itemName: item,subCategory,description,price,rating,customization,processing,stock,email,name}
+
+    console.log(newCraft);
+
+    // send data to the server
+    fetch('http://localhost:5000/crafts',{
+      method: 'POST',
+      headers: {
+        'content-type' : 'application/json'
+      },
+      body:JSON.stringify(newCraft)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+  }
+
+
+
   return (
     <div className="bg-blue-100 p-24">
       <h2 className="text-3xl font-extrabold text-center">Add Craft Item</h2>
-      <form>
+      <form onSubmit={handleAddCraft}>
         {/* form image & item_name */}
         <div className="md:flex ">
           <div className="form-control w-1/2">
@@ -19,7 +55,7 @@ const AddCraftItem = () => {
               <span className="label-text">Item Name</span>
             </label>
             <label className="input-group">
-              <input type="text" name="itemName" placeholder="Item Name" className="input input-bordered w-full" />
+              <input type="text" name="item" placeholder="Item Name" className="input input-bordered w-full" />
             </label>
           </div>
         </div>
@@ -30,7 +66,7 @@ const AddCraftItem = () => {
               <span className="label-text">Subcategory Name</span>
             </label>
             <label className="input-group">
-              <input type="text" name="subCategoryName" placeholder="Subcategory Name" className="input input-bordered w-full" />
+              <input type="text" name="subCategory" placeholder="Subcategory Name" className="input input-bordered w-full" />
             </label>
           </div>
           <div className="form-control w-1/2 ml-4">
@@ -38,7 +74,7 @@ const AddCraftItem = () => {
               <span className="label-text">Short Description</span>
             </label>
             <label className="input-group">
-              <input type="text" name="shortDescription" placeholder="Short Description" className="input input-bordered w-full" />
+              <input type="text" name="description" placeholder="Short Description" className="input input-bordered w-full" />
             </label>
           </div>
         </div>
@@ -76,7 +112,7 @@ const AddCraftItem = () => {
               <span className="label-text">Processing Time</span>
             </label>
             <label className="input-group">
-              <input type="text" name="processingTime" placeholder="Processing Time" className="input input-bordered w-full" />
+              <input type="text" name="processing" placeholder="Processing Time" className="input input-bordered w-full" />
             </label>
           </div>
         </div>
@@ -87,7 +123,7 @@ const AddCraftItem = () => {
               <span className="label-text">Stock Status</span>
             </label>
             <label className="input-group">
-              <input type="text" name="stockStatus" placeholder="Stock Status" className="input input-bordered w-full" />
+              <input type="text" name="stock" placeholder="Stock Status" className="input input-bordered w-full" />
             </label>
           </div>
           <div className="form-control w-1/2 ml-4">
@@ -95,7 +131,7 @@ const AddCraftItem = () => {
               <span className="label-text">User Email</span>
             </label>
             <label className="input-group">
-              <input type="text" name="userEmail" placeholder="User Email" className="input input-bordered w-full" />
+              <input type="text" name="email" placeholder="User Email" className="input input-bordered w-full" />
             </label>
           </div>
         </div>
@@ -105,7 +141,7 @@ const AddCraftItem = () => {
             <span className="label-text">User Name</span>
           </label>
           <label className="input-group">
-            <input type="text" name="userName" placeholder="User Name" className="input input-bordered w-full" />
+            <input type="text" name="name" placeholder="User Name" className="input input-bordered w-full" />
           </label>
         </div>
         <input type="submit" value="Add Coffee" className="btn btn-block btn-info mt-4" />
