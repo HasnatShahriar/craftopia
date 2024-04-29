510fbfcd04d1ab73e42fcf201d3d27753874e9ca
HasnatShahriar <shahriarhriday94@gmail.com>
2024-04-29 10:15:14 +0600

    added footer in all pages

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/shared/Navbar";
+import Footer from "../pages/shared/Footer";
 
 
 const Root = () => {
@@ -7,6 +8,7 @@ const Root = () => {
     <div className="max-w-6xl mx-auto">
       <Navbar/>
       <Outlet/>
+      <Footer/>
     </div>
   );
 };
